ab68733de2aa83c4afe843bb7ab7e4d5eb20be58
devashyapatil <devashyapatil@gmail.com>
2024-09-23 21:53:17 +0530

    Day15

---
+++ Day15(LongestSubstringWR).java
@@ -0,0 +1,25 @@
+class Day15 {
+    public static int lengthOfLongestSubstring(String s) {
+        if(s.length()==0)
+             return 0;
+
+        int maxi = Integer.MIN_VALUE;
+
+        Set<Character> set = new HashSet < > ();
+        int l = 0;
+        
+        for (int i=0; i<s.length(); i++) 
+        {
+            if (set.contains(s.charAt(i)))
+            {
+                while (l < i && set.contains(s.charAt(i))) {
+                    set.remove(s.charAt(l));
+                    l++;
+                }
+            }
+            set.add(s.charAt(i));
+            maxi = Math.max(maxans, i - l + 1);
+        }
+        return maxi;
+    }
+}
