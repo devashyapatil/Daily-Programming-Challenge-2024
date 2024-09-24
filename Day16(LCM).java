86802ba4f50c57a6e2fdb09bf2e0bae4724d21fe
devashyapatil <devashyapatil@gmail.com>
2024-09-24 19:50:31 +0530

    Day16

---
+++ Day16(LCM).java
@@ -0,0 +1,16 @@
+class Day16 {
+    
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b; 
+        return gcd(b % a, a); 
+    }
+    
+
+    public static int lcm(int a, int b)
+    {
+        return (a / gcd(a, b)) * b;
+    }
+    
+}
